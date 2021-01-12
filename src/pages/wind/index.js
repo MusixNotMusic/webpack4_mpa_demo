7381e9116cc768892bfe821b6e4a22f6d2353107
musixnotmusic <279601306@qq.com>
2021-01-12 17:31:46 +0800

    ...

--- src/pages/wind/index.js
+++ src/pages/wind/index.js
@@ -1,15 +1,13 @@
 import './index.scss'
 import * as d3 from "d3"
 import _ from 'lodash'
-import { zoomTransform } from 'd3'
-// import moment from 'moment'
-export default class Wind {
+export default class Wind{
     width = 900
     height = 600
-    margin = {top: 20, right: 30, bottom: 30, left: 20}
+    margin = {top: 20, right: 30, bottom: 30, left: 40}
     allWidth = 0
     allHeight = 0
-    testDataSize = 5000
+    testDataSize = 10000
     paths = [
         'M 4 2 Q 4 1.2 3.4 0.6 2.8 0 2 0 1.2 0 0.6 0.6 0 1.2 0 2 0 2.8 0.6 3.4 1.2 4 2 4 2.8 4 3.4 3.4 4 2.8 4 2 M 3.1 0.9 Q 3.6 1.3 3.6 2 3.6 2.7 3.1 3.1 2.6 3.6 2 3.6 1.3 3.6 0.8 3.1 0.4 2.7 0.4 2 0.4 1.3 0.8 0.9 1.3 0.4 2 0.4 2.6 0.4 3.1 0.9 Z',
         'M 5.3 0 L 3.7 0 3.7 30 5.3 30 5.3 0 Z',
@@ -38,14 +36,29 @@ export default class Wind {
     dateFormat = d3.timeFormat("%m-%d %H:%M")
     secondFormat = d3.timeFormat("%H:%M")
     color = d3.interpolateHslLong("red", "blue")
+    color1 = d3.interpolateHslLong("black", "steelblue")
+    colorRainbow = d3.interpolateRainbow
     transform = null
     domain = [0, 5000]
+    
+    yAxisOptions = {
+      tickSize: 6,
+      tickCount: 10,
+    }
+
+    xAxisOptions = {
+      tickCount: 13,
+      tickSize: 6
+    }
+
+    size = 46
     constructor(options = {}) {
         window.temp = this
         window.d3 = d3
         this.width  = ( options.width  || this.width ) 
         this.height = ( options.height || this.height )
         this.margin = options.margin || this.margin
+        this.domId =  options.domId
         this.allWidth = this.width + this.margin.right + this.margin.left
         this.allHeight = this.height + this.margin.top + this.margin.bottom
         this._render = _.throttle(this.render.bind(this), 50, {trailing: true})
@@ -54,10 +67,16 @@ export default class Wind {
 
     init() {
         this.getTestData()
-        this.createPanel()
+        // this.createPanel()
+        this.createContainer()
+            .createAxisLayer()
+            .createOverlayLayer()
+            .createProfileCanvasLayer()
+            .createEventLayer()
         this.registerEvent()
         this.initX()
         this.initY()
+        this.initBoxWidth()
         this.initScale()
         this.render()
     }
@@ -69,9 +88,87 @@ export default class Wind {
        this.ctx = this.canvas.getContext('2d')
        document.body.append(this.canvas)
     }
-
+    // 创建 容器层
+    createContainer() {
+      this.container = d3.select(`#${this.domId}`)
+        .append('div')
+        .attr('id', 'wind-container')
+        .style('position', 'relative')
+        .style('width', `${this.width + this.margin.left + this.margin.right}`)
+        .style('height', `${this.height + this.margin.top + this.margin.bottom}`)
+      return this
+    }
+    // 创建 坐标系 层 zoom=5
+    createAxisLayer() {
+      if (!d3.select(`#wind-container`).node()) {
+        throw Error('wind contaner dom not create!')
+      }
+      this.canvasAxis = d3.select(`#wind-container`)
+        .append('canvas')
+        .attr('id', 'wind-axis')
+        .style('position', 'absolute')
+        .style('top', '0')
+        .style('left', '0')
+        .style('z-index', 5).node()
+      this.canvasAxis.width = this.width + this.margin.left + this.margin.right
+      this.canvasAxis.height = this.height + this.margin.top + this.margin.bottom
+      this.contextAxis = this.canvasAxis.getContext('2d')
+      return this
+    }
+    // 创建 覆盖 层 zoom=6
+    createOverlayLayer() {
+      if (!d3.select(`#wind-container`).node()) {
+        throw Error('wind contaner dom not create!')
+      }
+      this.canvasOverlay = d3.select(`#wind-container`)
+        .append('canvas')
+        .attr('id', 'wind-overlay')
+        .style('position', 'absolute')
+        .style('top', `${this.margin.top}px`)
+        .style('left', `${this.margin.left + 1}px`)
+        .style('z-index', 6).node()
+      this.canvasOverlay.width = this.width
+      this.canvasOverlay.height = this.height - 1
+      this.contextOverlay = this.canvasOverlay.getContext('2d')
+      return this
+    }
+    // 创建 风羽图 层 zoom=7
+    createProfileCanvasLayer() {
+      if (!d3.select(`#wind-container`).node()) {
+        throw Error('wind contaner dom not create!')
+      }
+      this.canvasProfile = d3.select(`#wind-container`)
+        .append('canvas')
+        .attr('id', 'wind-profile')
+        .style('position', 'absolute')
+        .style('top', `${this.margin.top}px`)
+        .style('left', `${this.margin.left + 1}px`)
+        .style('z-index', 7).node()
+      this.canvasProfile.width = this.width
+      this.canvasProfile.height = this.height - 1
+      this.contextProfile = this.canvasProfile.getContext('2d')
+      return this
+    }
+    // 创建 事件动作管理 层 zoom=8
+    createEventLayer() {
+      if (!d3.select(`#wind-container`).node()) {
+        throw Error('wind contaner dom not create!')
+      }
+      this.canvasEvent = d3.select(`#wind-container`)
+        .append('canvas')
+        .attr('id', 'wind-event')
+        .style('position', 'absolute')
+        .style('top', 0)
+        .style('left', 0)
+        .style('z-index', 8).node()
+      this.canvasEvent.width = this.width + this.margin.left + this.margin.right
+      this.canvasEvent.height = this.height + this.margin.top + this.margin.bottom
+      this.contextEvent = this.canvasEvent.getContext('2d')
+      return this
+    }
+ 
     registerEvent() {
-        d3.select(this.canvas).call(
+        d3.select(this.canvasEvent).call(
             d3.zoom()
               .scaleExtent([0.5, 4])
               .on("zoom", () => { 
@@ -85,8 +182,7 @@ export default class Wind {
       this.x = d3
         .scaleLinear()
         .domain([d3.min(this.data, d => d.t), d3.max(this.data, d => d.t)])
-        // .nice()
-        .range([this.margin.left, this.width - this.margin.right])
+        .range([this.margin.left, this.width + this.margin.left])
     }
 
     initY() {
@@ -94,11 +190,17 @@ export default class Wind {
         .scaleLinear()
         .domain(this.domain)
         .nice()
-        .range([this.height - this.margin.bottom, this.margin.top])
+        // .range([this.height - this.margin.bottom, this.margin.top])
+        .range([this.height, this.margin.top])
     }
 
     initScale() {
-        this.sacle = 6000 / this.data.length > 0.8 ? 0.8: 6000 / this.data.length
+        this.scale = 1 / Math.log10(this.data.length) * 1.5
+        // this.scale = 1
+    }
+    
+    initBoxWidth () {
+      this.boxWidth = this.width / Math.ceil(this.data.length / this.size) 
     }
 
     getTestData() {
@@ -117,89 +219,56 @@ export default class Wind {
                   }
         })
     }
-
+    // 绘制 风羽
     drawWind(d, i) {
-      let boxWidth = 9
-      let boxHeight = 30
-      let _x = this.x(d.t) + this.margin.left + 20 - boxWidth / 2
-      let _y = this.y(d.hei) + this.margin.top - boxHeight / 2
-      let transform = d3.zoomTransform(this.canvas)
+      // let boxWidth = this.boxWidth
+      let boxHeight = 15
+      let transform = d3.zoomTransform(this.canvasEvent)
+      let _x = this.x(d.t)
+      let _y = this.y(d.hei + boxHeight / 2)
       let randPath =  this.paths[i % this.paths.length]
       let path = new Path2D(randPath)     
-      this.ctx.beginPath() 
-      this.ctx.translate(transform.applyX(_x), transform.applyY(_y))
-      this.ctx.scale(.5 * transform.k, .5 * transform.k)
-      this.ctx.rotate(d.dir / 180 * Math.PI)
-      this.ctx.fillStyle = this.color(i % 46 / 46)
-      this.ctx.fill(path)
-      this.ctx.resetTransform()
+      this.contextProfile.beginPath() 
+      this.contextProfile.translate(transform.applyX(_x), transform.applyY(_y))
+      this.contextProfile.scale(this.scale * transform.k, this.scale * transform.k)
+      this.contextProfile.rotate(d.dir / 180 * Math.PI)
+      this.contextProfile.fillStyle = this.color(i % 46 / 46)
+      this.contextProfile.fill(path)
+      this.contextProfile.resetTransform()
     }
-
-
-    // drawWind(d, i) {
-    //   let boxWidth = 20
-    //   let boxHeight = 40
-    //   let _x = this.x(d.t) + this.margin.left + 20 - boxWidth / 2
-    //   let _y = this.y(d.hei) + this.margin.top - boxWidth
-    //   let transform = d3.zoomTransform(this.canvas)
-    //   this.ctx.beginPath() 
-    //   this.ctx.fillStyle = this.color(i % 46 / 46)
-    //   this.ctx.rect(transform.applyX(_x), transform.applyY(_y), boxWidth, boxHeight)
-    //   this.ctx.fill()
-    // }
-
-
-    // drawXAxis() {
-    //     let ctx = this.ctx
-    //     let x = this.x
-    //     let transform = d3.zoomTransform(this.canvas)
-    //     var tickCount = 13,
-    //         tickSize = 6,
-    //         ticks = x.ticks(tickCount)
-    //     var yOffset = this.height + this.margin.top - this.margin.bottom 
-    //     var _x = (d) => { return  transform.applyX(x(d)) + this.margin.left } 
-    //     // var _x = (d) => { return x(d) + this.margin.left } 
-    //     ctx.beginPath()
-    //     ticks.forEach((d) => {
-    //       ctx.moveTo(_x(d), yOffset + 20);
-    //       ctx.lineTo(_x(d), yOffset + 20 + tickSize);
-    //     });
-    //     ctx.moveTo(this.margin.left + 20 , yOffset + 20);
-    //     ctx.lineTo(this.width + this.margin.left, yOffset + 20);
-    //     ctx.strokeStyle = "black";
-    //     ctx.stroke();
-      
-    //     ctx.textAlign = "center";
-    //     ctx.textBaseline = "top";
-    //     ctx.strokeStyle = "black";
-    //     ctx.fillStyle = "black";
-    //     // ctx.rotate(-Math.IP / 4)
-    //     ticks.forEach((d) => {
-    //       ctx.fillText(this.timeFormat(d), _x(d) , yOffset + 25 + tickSize)
-    //       ctx.rotate(-Math.IP / 4)
-    //     });
-    //     ctx.stroke();
-    // }
-
+    // 绘制叠加层
+    drawOverlay(d, i) {
+      let boxWidth = this.boxWidth
+      let boxHeight = 15
+      let transform = d3.zoomTransform(this.canvasEvent)
+      let _x = this.x(d.t) - boxWidth / 2
+      let _y = this.y(d.hei) - boxHeight
+      this.contextOverlay.beginPath() 
+      this.contextOverlay.fillStyle = this.color1(Math.log2(i) / Math.pow(i, 0.3) )
+      // this.contextOverlay.fillStyle = this.colorRainbow(Math.log2(i) / Math.pow(i, 0.3) + i / 20 )
+      // this.contextOverlay.fillStyle = this.colorRainbow(Math.random())
+      this.contextOverlay.rect(transform.applyX(_x), transform.applyY(_y), boxWidth * transform.k, boxHeight * transform.k)
+      this.contextOverlay.fill()
+    }
+    // 绘制坐标
     drawXAxis() {
-      let ctx = this.ctx
-      let transform = d3.zoomTransform(this.canvas)
+      let ctx = this.contextAxis
+      let transform = d3.zoomTransform(this.canvasEvent)
       let x = transform.rescaleX(this.x)
-      var yOffset = this.height + this.margin.top - this.margin.bottom 
+      // console.log(x.domain())
+      var yOffset = this.height + this.margin.top
       var _x = (d) => { return x(d) + this.margin.left }
-      var tickCount = 13,
-          tickSize = 6,
+      var tickCount = this.xAxisOptions.tickCount,
+          tickSize = this.xAxisOptions.tickSize,
           ticks = x.ticks(tickCount)
       ctx.beginPath()
       ticks.forEach((d) => {
-        ctx.moveTo(_x(d), yOffset + 20);
-        ctx.lineTo(_x(d), yOffset + 20 + tickSize);
+        ctx.moveTo(_x(d), yOffset);
+        ctx.lineTo(_x(d), yOffset + tickSize);
       });
-      ctx.moveTo(this.margin.left + 20 , yOffset + 20);
-      ctx.lineTo(this.width + this.margin.left, yOffset + 20);
+      ctx.moveTo(this.margin.left , yOffset);
+      ctx.lineTo(this.width + this.margin.left, yOffset);
       ctx.strokeStyle = "black";
-      ctx.stroke()
-      ctx.closePath()
 
       ctx.textAlign = "center";
       ctx.textBaseline = "top";
@@ -207,24 +276,22 @@ export default class Wind {
       ctx.fillStyle = "black";
       // ctx.rotate(-Math.PI / 4)
       ticks.forEach((d) => {
-        // console.log(transform.k,  this.dateFormat(d),  this.secondFormat(d))
-        ctx.fillText( ticks.length <= 12 ? this.dateFormat(d) : this.secondFormat(d), _x(d) , yOffset + 25 + tickSize)
+        ctx.fillText( ticks.length <= 12 ? this.dateFormat(d) : this.secondFormat(d), _x(d) , yOffset + tickSize)
       });
+      ctx.stroke();
     }
 
 
     drawYAxis() {
-        let ctx = this.ctx
-        let transform = d3.zoomTransform(this.canvas)
+        let ctx = this.contextAxis
+        let transform = d3.zoomTransform(this.canvasEvent)
         let y = transform.rescaleY(this.y)
-        var tickCount = 10,
-            tickSize = 6,
-            tickPadding = 3,
+        var tickCount = this.yAxisOptions.tickCount,
+            tickSize = this.yAxisOptions.tickSize,
             ticks = y.ticks(tickCount),
             tickFormat = y.tickFormat(tickCount);
-        var xOffset = this.margin.left + 20
-        // var _y = (d) => { return transform.applyY(y(d)) + this.margin.top + this.margin.bottom - 10 }
-        var _y = (d) => { return y(d)+ this.margin.top + this.margin.bottom - 10 }
+        var xOffset = this.margin.left
+        var _y = (d) => { return y(d)+ this.margin.top }
         // ticks
         ctx.beginPath();
         ticks.forEach((d) => {
@@ -232,7 +299,7 @@ export default class Wind {
           ctx.lineTo(xOffset - 6, _y(d));
         });
         ctx.moveTo(xOffset, this.margin.top);
-        ctx.lineTo(xOffset, this.height + this.margin.top - 10);
+        ctx.lineTo(xOffset, this.height + this.margin.top);
         ctx.strokeStyle = "black";
         ctx.stroke();
         
@@ -241,19 +308,38 @@ export default class Wind {
         ctx.textBaseline = "middle";
         ctx.fillStyle = "black";
         ticks.forEach((d) => {
-          ctx.fillText(tickFormat(d), xOffset - tickSize - tickPadding, _y(d));
+          ctx.fillText(tickFormat(d), xOffset - tickSize, _y(d));
         })
         ctx.stroke();
-      }
-
-    render() {
-        // console.log('render', d3.zoomTransform(this.canvas))
-        this.ctx.clearRect(0, 0, this.allWidth, this.allHeight);
-        this.ctx.beginPath();
-        this.data.forEach(this.drawWind.bind(this))
-        this.drawXAxis()  
-        this.drawYAxis()
     }
+    // 绘制坐标 
+    renderAxis() {
+      this.contextAxis.clearRect(0, 0, this.allWidth, this.allHeight);
+      this.drawXAxis()  
+      this.drawYAxis()
+    }
+
+     // 绘制 叠加层
+     renderOverlay() {
+      this.contextOverlay.clearRect(0, 0, this.allWidth, this.allHeight);
+      this.data.forEach(this.drawOverlay.bind(this))
+     }
+
+    // 绘制风羽图
+    renderProfile() {
+        // console.log('render', d3.zoomTransform(this.canvas))
+        this.contextProfile.clearRect(0, 0, this.allWidth, this.allHeight);
+        this.contextProfile.beginPath();
+        this.data.forEach(this.drawWind.bind(this))
+    }
+
+    // 整体绘制
+    render() {
+      this.renderAxis()
+      this.renderOverlay()
+      this.renderProfile()
+    }
+    
 
     getRatio() {
       var devicePixelRatio = window.devicePixelRatio || 1;
@@ -267,4 +353,4 @@ export default class Wind {
     }
 }
 
-new Wind()
+new Wind({domId: 'wind'})
